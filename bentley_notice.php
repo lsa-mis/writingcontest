40327084c53572008bd2d454cd903e49fddb496d
rsmokeUM <rsmoke@umich.edu>
2023-10-23 11:10:59 -0400

    add bentley notification partial

---
+++ bentley_notice.php
@@ -0,0 +1,5 @@
+<div class="p-2">
+  <strong>
+    By submitting an entry to any of these contests I understand that the Hopwood Program reserves the right to keep digital copies of winning manuscripts in a digital library maintained by the University of Michigan's Bentley Library.
+  </strong>
+</div>
