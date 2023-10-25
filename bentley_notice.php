86964508e923f5e1b66428060cb7e4d471be94fb
rsmokeUM <rsmoke@umich.edu>
2023-10-25 08:38:43 -0400

    add box around notification

--- bentley_notice.php
+++ bentley_notice.php
@@ -1,5 +1,5 @@
-<div class="p-2">
+<div style="border: thick double #F5F4C1; padding: 0px 2px;">
   <strong>
-    By submitting an entry to any of these contests I understand that the Hopwood Program reserves the right to keep digital copies of winning manuscripts in a digital library maintained by the University of Michigan's Bentley Library.
+    By submitting an entry to any of these contests you understand that the Hopwood Program reserves the right to keep digital copies of winning manuscripts in a digital library maintained by the University of Michigan's Bentley Library.
   </strong>
 </div>
